bf1451862c6803a3d1be0b27a554561da9ed42fc
saiteja <saitejavinukond684@gmail.com>
2025-06-10 22:47:54 +0530

    updated

--- Travel/urls.py
+++ Travel/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from Travel import views
 urlpatterns = [
- 
+    path('',views.home,name='home'),
     path('home',views.home, name='home'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
     path('login/',views.login_view,name='login_view'),
     path('register/',views.register_view,name='register_view'),
-
     path('forgot_password/', views.forgot_password, name='forgot_password'),
 ]
