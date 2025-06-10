dffbb2f32f13251aac0fac22c32007cbca12d4b9
saiteja <saitejavinukond684@gmail.com>
2025-06-10 18:24:48 +0530

    hello

--- Travel/urls.py
+++ Travel/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('home/',views.home, name='home'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
-    path('login/',views.login_view,name='login'),
-    path('register/',views.register_view,name='register'),
+    path('login/',views.login_view,name='login_view'),
+    path('register/',views.register_view,name='register_view'),
 ]
