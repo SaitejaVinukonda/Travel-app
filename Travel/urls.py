32626cbe6256e8a6025a9213ace7916c0202c7b4
SuryaPrakashBolloju <suryaprakashbolloju@gmail.com>
2025-06-11 12:18:20 +0530

    updated

--- Travel/urls.py
+++ Travel/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from Travel import views
 urlpatterns = [
     path('',views.home,name='home'),
-    path('home',views.home, name='home'),
+    path('home/',views.home, name='home'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
     path('login/',views.login_view,name='login_view'),
